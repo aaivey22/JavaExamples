e9c463fe4a86e451ce75f1a67bdb90f40d4ffdde
aaivey22 <angelawings22@gmail.com>
2023-08-08 13:05:38 -0400

    expirimenting with object creation

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -2,6 +2,26 @@ package com.example.lib;
 
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Cycle myCycle = new Cycle();
+        myCycle.cycleStart = 27;
+        myCycle.cycleEnd = 1;
+        myCycle.prediction();
+
     }
+
+}
+class Cycle {
+    // states
+    int cycleStart;
+    int cycleEnd;
+
+    int nextCycle;
+
+    // methods
+    void prediction() {
+        nextCycle += 25;
+        System.out.println("next cycle starts: " + nextCycle);
+    }
+
 }
