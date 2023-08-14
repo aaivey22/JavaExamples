06931e4da7e105ea18b9f2537e62065d9af3990e
aaivey22 <angelawings22@gmail.com>
2023-08-14 18:49:13 -0400

    adding scanner object to receive user input for start and end of cycle.

--- lib/src/main/java/com/example/lib/MyClass.java
+++ lib/src/main/java/com/example/lib/MyClass.java
@@ -1,13 +1,21 @@
 package com.example.lib;
 
+import java.util.Scanner;
+
 public class MyClass {
     public static void main(String[] args) {
 
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter the first calendar date of your cycle: ");
+        int n1 = s1.nextInt();
+        //int n1 = 1;
+        System.out.println("Enter the last calendar date of your cycle: ");
+        int n2 = s1.nextInt();
+        //int n2 = 2;
         Cycle myCycle = new Cycle();
-        myCycle.cycleStart = 27;
-        myCycle.cycleEnd = 1;
+        myCycle.cycleStart = n1;
+        myCycle.cycleEnd = n2;
         myCycle.prediction();
-
     }
 
 }
@@ -16,11 +24,14 @@ class Cycle {
     int cycleStart;
     int cycleEnd;
 
-    int nextCycle;
+    int nextCycle = 0;
 
     // methods
     void prediction() {
         nextCycle += 25;
+        System.out.println("Your last cycle began on: " + cycleStart);
+        System.out.println("Your last cycle ended on: " + cycleEnd);
+
         System.out.println("next cycle starts: " + nextCycle);
     }
 
